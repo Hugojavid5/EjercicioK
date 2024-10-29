bf4d95040dbb9f23bac6531d935867fe163ea2ee
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-29 19:39:37 +0100

    dibujo terminado

---
+++ src/main/java/org/hugo/ejerciciok/RelojController.java
@@ -0,0 +1,19 @@
+package org.hugo.ejerciciok;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class RelojController {
+    @FXML
+    private ImageView imgHorasDerecha;
+    @FXML
+    private ImageView imgHorasIzquierda;
+    @FXML
+    private ImageView imgMinutosDerecha;
+    @FXML
+    private ImageView imgMinutosIzquierda;
+    @FXML
+    private ImageView imgSegundosDerecha;
+    @FXML
+    private ImageView imgSegundosIzquierda;
+}
